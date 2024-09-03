f1ce68db48dabc027374cd7d347bd03a3d79b674
archcloudlabs <1250113+archcloudlabs@users.noreply.github.com>
2024-09-03 19:36:01 -0400

    Refactoring main

--- main.go
+++ main.go
@@ -1,34 +1,12 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"io"
-	"net/http"
 	"strings"
 	"os"
 	"github.com/go-git/go-git/v5"
 )
 
-type RSS struct {
-	XMLName xml.Name `xml:"rss"`
-	Channel Channel `xml:"channel"`
-}
-
-
-type Channel struct {
-	Title string `xml:"title"`
-	Description string `xml:"description"`
-	Link string `xml:"link"`
-	Items []Item `xml:"item"`
-}
-
-type Item struct {
-	Title string `xml:"title"`
-	Link string `xml:"link"`
-	Description string `xml:"description"`
-	PubData string `xml:"PubData"`
-}
 
 func errCheck(err error) {
 	if err != nil {
@@ -37,28 +15,11 @@ func errCheck(err error) {
 	}
 }
 
-func fetchAndParseRSS(url string) (*RSS, error){
-
-	resp, err := http.Get(url)
-	errCheck(err)
-
-	defer resp.Body.Close() // close the handle later.
-
-	data, err := io.ReadAll(resp.Body)
-	errCheck(err)
-
-	var rss RSS
-	err = xml.Unmarshal(data, &rss)
-	errCheck(err)
-
-	return &rss, nil
-}
-
 
 func main() {
 
 	url := "http://aur.archlinux.org/rss"
-	rss,err := fetchAndParseRSS(url)
+	rss,err := FetchAndParseRSS(url)
 	errCheck(err)
 
 	fmt.Printf("Channel: %s\nDescription: %s\n\n", rss.Channel.Title, rss.Channel.Description)
@@ -68,7 +29,7 @@ func main() {
 		repo := strings.Replace(item.Link, "packages/", "", 1)
 
 		// TODO: check the git clone, and send a message to a repo or something.
-		_, _:= git.PlainClone(item.Title + ".git", false, &git.CloneOptions{
+		_, _= git.PlainClone(item.Title + ".git", false, &git.CloneOptions{
 			URL: repo,
 			Progress: os.Stdout,
 		})
